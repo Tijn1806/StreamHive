6ae19bb756faebe11ff8da27f27c433a16c80946
Tijn1806 <Tijn@kromjong.nl>
2026-06-04 09:41:44 +0200

    Upload pagina werkend

--- public/main-page/index.php
+++ public/main-page/index.php
@@ -1,5 +1,13 @@
 <?php
-    session_start();
+session_start();
+
+require_once(__DIR__ . '/../../core/Database.php');  //database connectie wordt geladen
+require_once(__DIR__ . '/../../app/models/video.php');  //video model wordt geladen
+
+$videoModel = new Video($pdo);  
+
+// Alle video's ophalen
+$videos = $videoModel->all(); 
 ?>
 
 <!DOCTYPE html>
@@ -34,8 +42,21 @@
         </form>
     </nav>
 
-    <main>
-        <h2>Recommended</h2>
-    </main>
+    <section class="recommended">
+    <h2>Recommended</h2>
+
+    <div class="video-grid">
+        <?php foreach ($videos as $video): ?>
+            <div class="video-card">
+                <video width="300" controls>
+                    <source src="../../uploads/videos/<?= htmlspecialchars($video['filename']) ?>" type="video/mp4">
+                    Je browser ondersteunt deze video niet.
+                </video>
+
+                <h3><?= htmlspecialchars($video['title']) ?></h3>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
 </body>
 </html>
